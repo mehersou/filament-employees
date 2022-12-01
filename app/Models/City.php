83505b0e7a50fc2510970e4afd54421163245725
mehersou <109005408+mehersou@users.noreply.github.com>
2022-12-01 23:08:29 +0100

    crud: employees,city, country, department

---
+++ app/Models/City.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\State;
+use App\Models\Employee;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class City extends Model
+{
+    use HasFactory;
+
+    protected $fillable=['state_id','name'];
+
+    public function state()
+    {
+        return $this->belongsTo(State::class);
+    }
+
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
+    }
+}
