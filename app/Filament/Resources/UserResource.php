9a8857cb5854993e3595680b063a27c60b63dadc
mehersou <109005408+mehersou@users.noreply.github.com>
2022-12-02 08:51:44 +0100

    user management

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -24,6 +24,7 @@ class UserResource extends Resource
     protected static ?string $model = User::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-collection';
+    
 
     public static function form(Form $form): Form
     {
